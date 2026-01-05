e2ed6cba9ce327cab1759e492097b0f79ed706f0
asmaa-afk <Asmaa@tadafuq.ae>
2026-01-05 17:41:32 +0200

    createpackageSlug

--- src/SlugServiceProvider.php
+++ src/SlugServiceProvider.php
@@ -11,6 +11,11 @@ class SlugServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
          $this->loadMigrationsFrom(__DIR__ . '/../src/database/migrations');
         $this->loadViewsFrom(__DIR__ . '/../src/views','asmaa/slug');
+
+        $this->publishes([
+    __DIR__ . '/../src/database/migrations' => database_path('migrations'),
+], 'slug-migrations');
+
     }
 
     public function register()
